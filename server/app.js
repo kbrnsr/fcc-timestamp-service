1efe95e070cef12741f10892d2d52a47c6775caa
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-08-11 10:16:11 +0200

    remove / and /users route

--- server/app.js
+++ server/app.js
@@ -3,8 +3,6 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
 import apiRouter from './routes/api';
 
 const app = express();
@@ -15,8 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api/date', apiRouter);
 
 export default app;
